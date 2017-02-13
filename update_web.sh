9fed00e3cee5d351c3cb5da387939768ffdaa013
Dima D <dergachovda@gmail.com>
2017-02-13 13:03:22 +0200

    update update_web.sh

--- update_web.sh
+++ update_web.sh
@@ -1,9 +1,18 @@
+echo "delete /app & /img"
 rm -r ./src/main/resources/static/app
 rm -r ./src/main/resources/static/img
 
 cd ../web
 git pull origin master
-cp -r ./dist ../ecity/src/main/resources/static/app
 
+echo "copy web project to /resources/static/app"
+cp -r ./dist ../ecity/src/main/resources/static/app
 cd ../ecity/src/main/resources/static/
+
+echo "move img to /resources/static/"
 mv app/img img
+
+echo "copy favicon.ico to ./app"
+cp ./favicon.ico ./app
+
+
